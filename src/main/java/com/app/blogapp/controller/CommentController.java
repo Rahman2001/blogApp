8537c6b5f28b8ddb3b700c714bf4b30fe0ba34dc
Rahman2001 <rahmanrejepov777@gmail.com>
2024-06-18 22:37:50 +0300

    3rd criteria implemented

--- src/main/java/com/app/blogapp/controller/CommentController.java
+++ src/main/java/com/app/blogapp/controller/CommentController.java
@@ -1,4 +1,29 @@
 package com.app.blogapp.controller;
 
+import com.app.blogapp.domain.Comment;
+import com.app.blogapp.domain.Post;
+import com.app.blogapp.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comment")
 public class CommentController {
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/getCommentsByPost/{post_id}")
+    public ResponseEntity<List<Comment>> getAllComments(@PathVariable("post_id") long post_id) {
+        List<Comment> comments = this.commentService.findCommentsByPostId(post_id);
+        return comments != null && !comments.isEmpty() ?
+                ResponseEntity.ok(comments) : ResponseEntity.notFound().build();
+
+    }
 }
--- src/main/java/com/app/blogapp/service/CommentService.java
+++ src/main/java/com/app/blogapp/service/CommentService.java
@@ -1,9 +1,12 @@
 package com.app.blogapp.service;
 
 
+import com.app.blogapp.domain.Comment;
 import com.app.blogapp.repo.CommentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentService {
@@ -12,5 +15,9 @@ public class CommentService {
     @Autowired
     public CommentService(CommentRepo commentRepo) {
         this.commentRepo = commentRepo;
+    }
+
+    public List<Comment> findCommentsByPostId(long postId) {
+        return this.commentRepo.findAllByPostId(postId);
     }
 }
