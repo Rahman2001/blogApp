a74dc11a7b6a933c5b317bc41b442c2e35136e0d
Rahman2001 <rahmanrejepov777@gmail.com>
2024-06-18 20:54:36 +0300

    1st criteria implemented

--- src/main/java/com/app/blogapp/controller/UserController.java
+++ src/main/java/com/app/blogapp/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.app.blogapp.controller;
 
+import com.app.blogapp.domain.User;
+import com.app.blogapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> users = this.userService.findAll();
+        return users != null && !users.isEmpty() ?
+                ResponseEntity.ok(users) : ResponseEntity.badRequest().build();
+    }
 }
--- src/main/java/com/app/blogapp/service/UserService.java
+++ src/main/java/com/app/blogapp/service/UserService.java
@@ -1,8 +1,11 @@
 package com.app.blogapp.service;
 
+import com.app.blogapp.domain.User;
 import com.app.blogapp.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -11,6 +14,10 @@ public class UserService {
     @Autowired
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
     }
 
 }
